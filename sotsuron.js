6ca602cc25ab622ac695daa79951043ed17cadff
hoto <hoto17296@gmail.com>
2014-01-16 19:43:20 +0900

    initial

---
+++ sotsuron.js
@@ -0,0 +1,27 @@
+$(function(){
+
+  function update(){
+    var now  = new Date().getTime();
+    var dead = new Date(deadline).getTime();
+    var diff = Math.floor((dead - now) / 1000);
+    var remain = {
+      day  : Math.floor(diff / 86400),
+      hour : Math.floor(diff / 3600 % 24),
+      min  : Math.floor(diff / 60 % 60),
+      sec  : Math.floor(diff % 60)
+    };
+    var str = remain.day  + ' 日 '
+            + remain.hour + ' 時間 '
+            + remain.min  + ' 分 '
+            + remain.sec  + ' 秒 ';
+    $('.remain').html(str);
+  }
+
+  setInterval(update, 1000);
+
+  update();
+  $('.progress-per').text(progress);
+  $('.progress-bar')
+    .attr('aria-valuenow', progress)
+    .css('width', progress+'%');
+});
